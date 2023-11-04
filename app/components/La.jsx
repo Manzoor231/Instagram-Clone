d3440c7d2861da352e2f02e53d80a794ba7df442
Manzoor Strange <strangemia7001@gmail.com>
2023-11-05 02:37:44 +0500

    Instagram Profile Clone

---
+++ app/components/La.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function La() {
+  return (
+    <>
+    <div className='flex flex-col h-screen'>
+        <div>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+        </div>
+        <div className='grow'>
+            <h1>Learning something new</h1>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default La
---
+++ app/page.js
@@ -0,0 +1,13 @@
+import MainContent from "./components/MainContent";
+import Sidebar from "./components/Sidebar";
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex ">
+        <Sidebar />
+        <MainContent />
+      </div>
+    </>
+  );
+}
